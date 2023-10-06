c8f2773f981d0d7e272de43bc23b434657ca0e45
Alfred Christo W <122668641+Alfred-Christo@users.noreply.github.com>
2023-10-06 16:36:22 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,30 +1,66 @@
 import streamlit as st
-from transformers import AutoModelForSequenceClassification
+from langchain.vectorstores import Chroma
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
+import torch
+from langchain.llms import HuggingFacePipeline
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.chains import RetrievalQA
 
-# Load the Mini-LM model
-model = AutoModelForSequenceClassification.from_pretrained("google/mini-lm-tf2-base")
+checkpoint = "MBZUAI/LaMini-Flan-T5-783M"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+base_model = AutoModelForSeq2SeqLM.from_pretrained(
+    checkpoint, device_map="auto", torch_dtype=torch.float32
+)
+embeddings = SentenceTransformerEmbeddings(
+    model_name="sentence-transformers/multi-qa-mpnet-base-dot-v1"
+)
+db = Chroma(persist_directory="ipc_vector_data", embedding_function=embeddings)
+pipe = pipeline(
+    "text2text-generation",
+    model=base_model,
+    tokenizer=tokenizer,
+    max_length=512,
+    do_sample=True,
+    temperature=0.3,
+    top_p=0.95,
+)
+local_llm = HuggingFacePipeline(pipeline=pipe)
 
-# Define a function to generate legal advice
-def generate_legal_advice(query):
-    # Encode the query
-    encoded_input = model.prepare_inputs_for_classification(query, return_tensors="tf")
+qa_chain = RetrievalQA.from_chain_type(
+    llm=local_llm,
+    chain_type="stuff",
+    retriever=db.as_retriever(search_type="similarity", search_kwargs={"k": 2}),
+    return_source_documents=True,
+)
 
-    # Generate the legal advice
-    legal_advice = model.generate(**encoded_input, max_length=100, num_beams=5)
+chat_history = []
 
-    # Decode the legal advice
-    legal_advice = model.decode(legal_advice, skip_special_tokens=True)
 
-    return legal_advice
+def chat():
+    """Returns a response to the user input."""
+    user_input = st.session_state["input_query"]
+    bot_response = qa_chain({"query": user_input})
+    bot_response = bot_response["result"]
+    response = ""
+    for letter in "".join(bot_response):
+        response += letter + ""
+    chat_history.append((user_input, response))
+    st.session_state["chat_history"] = chat_history
+    st.session_state["input_query"] = ""
+    return response
 
-# Create a Streamlit app
-st.title("AI Powered Legal Assistant")
 
-# Get the user's query
-query = st.text_input("Enter your legal question:")
+st.title("Chatbot")
 
-# Generate the legal advice
-legal_advice = generate_legal_advice(query)
+st.session_state["chat_history"] = chat_history
+st.session_state["input_query"] = ""
 
-# Display the legal advice
-st.write(legal_advice)
+st.text("Chat history:")
+st.write(st.session_state["chat_history"])
+
+input_query_element = st.text_area("Input", value=st.session_state["input_query"])
+st.button("Submit", on_click=chat)
+
+if __name__ == "__main__":
+    st.run()
